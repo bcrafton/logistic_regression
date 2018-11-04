d64291d47506123787f3ccbc2e3661fbb9d9b003
Brian Crafton <crafton.b@husky.neu.edu>
2018-11-03 21:23:38 -0400

    memristor muri stuff

--- mnist.py
+++ mnist.py
@@ -7,15 +7,20 @@ import pickle
 import argparse
 import keras
 from keras.datasets import mnist
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 #######################################
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epochs', type=int, default=10)
-parser.add_argument('--alpha', type=float, default=1e-4)
-parser.add_argument('--scale', type=float, default=2.0)
-parser.add_argument('--low', type=float, default=1e-2)
+parser.add_argument('--alpha', type=float, default=0.0005)
+parser.add_argument('--scale', type=float, default=4.0)
+parser.add_argument('--low', type=float, default=0.05)
+parser.add_argument('--pca', type=int, default=350)
 args = parser.parse_args()
+
+print (args)
 
 #######################################
 
@@ -27,15 +32,39 @@ NUM_CLASSES = 10
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(TRAIN_EXAMPLES, 784)
-x_test = x_test.reshape(TEST_EXAMPLES, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+#######################################
 
 y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
+
+x_train = x_train.reshape(TRAIN_EXAMPLES, 784)
+x_train = x_train.astype('float32')
+# x_train /= 255
+# x_train = x_train - np.average(x_train, axis=0)
+
+#######################################
+
 y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
+
+x_test = x_test.reshape(TEST_EXAMPLES, 784)
+x_test = x_test.astype('float32')
+# x_test /= 255
+# x_test = x_test - np.average(x_test, axis=0)
+
+#######################################
+
+scaler = StandardScaler()
+scaler.fit(x_train)
+
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+#######################################
+
+pca = PCA(n_components=args.pca)
+pca.fit(x_train)
+
+x_train = pca.transform(x_train)
+x_test = pca.transform(x_test)
 
 #######################################
 
@@ -50,7 +79,7 @@ def sigmoid(x):
 
 print (args.epochs, args.alpha, args.scale, args.low)
 
-LAYER1 = 784
+LAYER1 = args.pca
 LAYER2 = 10
 
 # remember there is a sigmoid ... so it makes things a little nicer
@@ -95,11 +124,12 @@ for epoch in range(args.epochs):
             correct += 1
         
     acc = 1.0 * correct / TEST_EXAMPLES
-    print ("accuracy: " + str(acc))
     accs.append(acc)
+    
+    print ("accuracy: " + str(acc))
     print (np.min(weights), np.max(weights), np.average(weights), np.std(weights))
 
-name = "./results/epochs_%d_alpha_%f_scale_%f_low_%f.npy"% (args.epochs, args.alpha, args.scale, args.low)
+name = "./results/epochs_%d_alpha_%f_scale_%f_low_%f_pca_%d.npy"% (args.epochs, args.alpha, args.scale, args.low, args.pca)
 np.save(name, accs)
 
     
